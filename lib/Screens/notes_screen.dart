6abe223d5ee12a060b4599d75bd97c819f5e9d29
BassemSiam <bassemsiam644@gmail.com>
2023-09-10 19:08:35 +0300

    create showNotes method

--- lib/Screens/notes_screen.dart
+++ lib/Screens/notes_screen.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app/Components/Widgets/Custom_button_sheet.dart';
 import 'package:note_app/Components/Widgets/widgets.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 
 class NoteScreen extends StatelessWidget {
   const NoteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const NoteViweBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20)
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: BlocConsumer<NotesCubit, NotesStates>(
+        listener: (context, state) {
+        },
+        builder: (context, state) {
+          return Scaffold(
+            body: const NoteViweBody(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  context: context,
+                  builder: (context) {
+                    return CustomBottomSheet();
+                  },
+                );
+              },
+              child: const Icon(Icons.add),
             ),
-            context: context,
-            builder: (context) {
-              return CustomBottomSheet();
-            },
           );
         },
-        child:const Icon(Icons.add),
       ),
     );
   }
--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/Components/Consts/constants.dart';
 import 'package:note_app/Models/note_model.dart';
 
 part 'notes_state.dart';
@@ -6,4 +8,17 @@ part 'notes_state.dart';
 class NotesCubit extends Cubit<NotesStates> {
   NotesCubit() : super(NotesInitialState());
   static NotesCubit get(context) => BlocProvider.of(context);
+
+  showNotes() async {
+    emit(NotesLoadingState());
+
+    try {
+      var notesBox = Hive.box<NoteModel>(noteBox);
+
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccessState(notes));
+    } catch (error) {
+      emit(NotesErrorState(error.toString()));
+    }
+  }
 }
